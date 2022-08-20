11d67fb4e6d59aa995e1c7a38bf8de5ad2736405
epicgl <mukithasan58@gmail.com>
2022-08-20 17:34:15 +0600

    whitenoice fixed

--- main/settings.py
+++ main/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,6 +129,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static/',
 )
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/img/'
 
